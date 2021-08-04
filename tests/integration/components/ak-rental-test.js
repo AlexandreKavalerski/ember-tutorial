0c47c3c1ad509ae8ee1d12b2acc0fd9dc63c4379
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2021-08-04 08:32:14 -0300

    feat: add model hook to ak-rental component

--- tests/integration/components/ak-rental-test.js
+++ tests/integration/components/ak-rental-test.js
@@ -7,10 +7,29 @@ module('Integration | Component | ak-rental', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders information about a rental property', async function (assert) {
-    await render(hbs`<AkRental />`);
+    this.setProperties({
+      rental: {
+        title: 'Scooby House',
+        owner: 'Veruca Salt',
+        city: 'San Francisco',
+        location: {
+          lat: 37.7749,
+          lng: -122.4194,
+        },
+        category: 'Estate',
+        type: 'Standalone',
+        bedrooms: 15,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        description:
+          'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+      },
+    });
+
+    await render(hbs`<AkRental @rental={{this.rental}} />`);
 
     assert.dom('article').hasClass('rental');
-    assert.dom('article h3').hasText('Grand Old Mansion');
+    assert.dom('article h3').hasText('Scooby House');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
